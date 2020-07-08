74873a61bd313dac4f40d418f188050ca31257e9
Michael Mekuleyi <mekuleyimichael@gmail.com>
2020-07-08 15:01:22 +0100

    initial commit

--- 1-getting-started/2-timers/4-challenge1.js
+++ 1-getting-started/2-timers/4-challenge1.js
@@ -1,9 +1,11 @@
-const theOneFunc = () => {};
-
-setTimeout(theOneFunc, 4 * 1000);
+const theOneFunc = (n) => {
+  console.log(`Hello afer ${n} seconds`);
+};
 
 // Hello after 4 seconds
+setTimeout(theOneFunc, 4 * 1000, 4);
 
 // Hello after 8 seconds
+setTimeout(theOneFunc, 8 * 1000, 8);
 
 // You can define only ONE function
--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,13 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+var i = 0;
+const five = () => {
+  i++;
+  if (i === 5) {
+    clearInterval(interval);
+  }
+  console.log("Hello World");
+};
+
+var interval = setInterval(five, 1000);
